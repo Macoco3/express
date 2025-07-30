96d26011f90931a29df628301fc7d11fdcfb62c8
Satoh Mako <evisama7@gmail.com>
2025-07-31 00:57:46 +0900

    add env

--- routes/cat.js
+++ routes/cat.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var router = express.Router();
 const request = require("request");
+const cors = require("cors"); // corsミドルウェアを追加
+
+// corsミドルウェアを使用
+router.use(cors());
 
 router.get("/", async (req, res) => {
   request(
